d12377730de4fb175f4e0ffedbd2aeb7f2a88a27
Chris Cooper <cjcooper252@gmail.com>
2016-11-18 09:14:09 +0000

    start records a journey

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,19 +1,24 @@
+require_relative 'journey.rb'
+require_relative 'station.rb'
+
 class JourneyLog
 
-  attr_reader :current_journey
+  attr_reader :current_journey, :journeys
 
   def initialize(journey_klass)
     @journey_klass = journey_klass
-    @log = []
+    @journeys = []
   end
 
   def start(station)
-    @current_journey = @journey_klass.new(station)
+      @current_journey = @journey_klass.new(station)
+      @journeys << @current_journey
   end
 
   def finish(station)
     @current_journey.end(station)
-    @log << current_journey
+    @journeys << current_journey
+    @current_journey = nil
   end
 
 end
